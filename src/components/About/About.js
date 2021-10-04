d7c147e2bd30ffe6a36ed37a2b485c37dd99d9d5
onuproy <anupme01@gmail.com>
2021-10-04 21:59:50 +0600

    404 page update and css update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,6 +12,19 @@ const About = () => {
                         </div>
                    </div>
                </div>
+               <div className="row about-content-image">
+                   <div className="col-md-6">
+                       <div className="about-content">
+                          <h4> Coder IT Solution</h4>
+                          <p>Coder IT Solution is a professional Web, E-Commerce, and software development company, providing Web Courses and an extent of affordable custom website design, E-Commerce, in fact, all kind of website design services to clients all over the world.</p>
+                       </div>
+                   </div>
+                   <div className="col-md-6">
+                       <div className="about-image">
+                           <img src="https://coderitsolution.com/wp-content/themes/CoderIT/assets/images/getSubscribe-img.png" alt="About iamge" />
+                       </div>
+                   </div>
+               </div>
            </div>
        </div>
     );
